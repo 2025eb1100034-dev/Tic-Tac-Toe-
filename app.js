c74b42d1ff22a4f5dd47fa3d8292960cab47e072
2025eb1100034-dev <2025eb1100034@bitspilani-digital.edu.in>
2026-03-17 20:28:12 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,70 @@
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector("#reset-btn");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
+let newBtn = document.querySelector("#new-btn");
+let winPatterns = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+let turn0 = true;
+
+boxes.forEach((box) => {
+    box.addEventListener("click",() => {
+        if(turn0){
+            box.innerText = "O";
+            turn0 = false;
+        } else{
+            box.innerText = "X";
+            turn0 = true;
+        }
+        box.disabled = true;
+        checkWinner();
+
+    });
+
+});
+
+function checkWinner(){
+    for(let pattern of winPatterns){
+        let pos1val = boxes[pattern[0]].innerText;
+        let pos2val =boxes[pattern[1]].innerText;
+        let pos3val = boxes[pattern[2]].innerText;
+        if(pos1val != "" && pos2val != "" && pos3val != ""){
+            if(pos1val == pos2val && pos2val == pos3val){
+                showWinner(pos1val);
+            }
+        }
+    }
+
+}
+function showWinner(winner){
+    msg.innerText = `Congratulations, Winner is ${winner}`;
+    msgContainer.classList.remove("hide");
+    disableBtns();
+}
+
+function disableBtns(){
+    for(let box of boxes){
+        box.disabled = true;
+    }
+}
+
+let resetGame = () => {
+     turn0 = true;
+    enableBtns();
+    msgContainer.classList.add("hide");
+
+};
+
+
+let enableBtns = () => {
+    for(let box of boxes){
+          box.disabled = false;
+        box.innerText = ""
+    
+
+    }
+      
+
+}
+
+newBtn.addEventListener("click",resetGame);
+resetBtn.addEventListener("click",resetGame);
